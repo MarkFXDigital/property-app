86a3a78b0c4a66ebbb9a9262a3cb390e6570551b
Mark Marley <mark@fxdigital.uk>
2022-05-01 19:34:49 +0100

    Graph fully working with dynamic data from api. Graph styling also done apart from colors

--- boilerplate.js
+++ boilerplate.js
@@ -11,4 +11,74 @@ const boilerplate = () => {
 
 const styles = StyleSheet.create({});
 
+<VictoryChart
+// width={moderateScale(320)}
+// height={moderateScaleVertical(220)}
+>
+  <VictoryGroup offset={15} colorScale={"qualitative"}>
+    <VictoryBar
+      data={[
+        { x: 1, y: 1 },
+        { x: 2, y: 3 },
+        { x: 3, y: 3 },
+      ]}
+      animate={{
+        duration: 1000,
+        onLoad: { duration: 1000 },
+      }}
+      cornerRadius={6}
+      style={{
+        data: {
+          // fill: colors.skyBle,
+        },
+      }}
+      barWidth={10}
+    />
+
+    <VictoryBar
+      data={[
+        { x: 1, y: 1 },
+        { x: 2, y: 2 },
+        { x: 3, y: 3 },
+      ]}
+      animate={{
+        duration: 2000,
+        onLoad: { duration: 1000 },
+      }}
+      cornerRadius={6}
+      style={{
+        data: {
+          // fill: colors.primary,
+        },
+      }}
+      barWidth={10}
+      labelComponent={CenteredLabel}
+    />
+    <VictoryAxis
+      tickValues={["70% Range", "Aug", "Sep"]}
+      style={{
+        tickLabels: {
+          fontSize: 5,
+          padding: 1,
+          angle: 10,
+          verticalAnchor: "middle",
+          textAnchor: "start",
+        },
+        //   tickLabels: { color: colors.blackL },
+        //   ticks: { color: colors.blackL },
+      }}
+    />
+    <VictoryAxis
+      dependentAxis={true}
+      tickValues={["₹0K", "₹10K", "₹20K", "₹30K"]}
+      style={
+        {
+          //   tickLabels: { fill: colors.blackL },
+          //   ticks: { stroke: colors.blackL },
+        }
+      }
+    />
+  </VictoryGroup>
+</VictoryChart>;
+
 export default boilerplate;
--- src/Screens/SearchScreen.js
+++ src/Screens/SearchScreen.js
@@ -6,15 +6,13 @@ import {
   Text,
   TouchableOpacity,
   ScrollView,
+  Dimensions,
 } from "react-native";
 import PropertyLogo from "../Components/PropertyLogo";
 import {
   VictoryBar,
   VictoryChart,
-  VictoryTheme,
   VictoryGroup,
-  VictoryLabel,
-  VictoryStack,
   VictoryAxis,
   CenteredLabel,
 } from "victory-native";
@@ -22,19 +20,14 @@ import {
 const SearchScreen = () => {
   // To store response data
   const [data, setData] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   // Search Params
   const [postcode, setPostcode] = useState("SW12");
   const [bedroomNum, setBedroomNum] = useState("2");
 
-  //   const fetchPriceSearch = async (e) => {
-  //     try {
-  //       const response = await fetch(
-  //         `https://api.propertydata.co.uk/prices?key=TORGPUR3KY&postcode=${postcode}&bedrooms=${bedroomNum}`
-  //       );
-  //       console.log(response);
-  //     } catch {}
-  //   };
+  const chartHeight = Dimensions.get("window").height * 0.4;
+  const chartWidth = Dimensions.get("window").width;
 
   const fetchPriceSearch = async () => {
     try {
@@ -43,109 +36,191 @@ const SearchScreen = () => {
       );
       const json = await response.json();
 
-      console.log(json.data["70pc_range"][0]);
-      console.log(json.data["70pc_range"][1]);
       setData(json);
-
-      return setData;
+      setIsLoaded(true);
     } catch (error) {
       console.error(error);
     }
   };
 
-  return (
-    <View style={styles.mainContainer}>
-      <PropertyLogo />
-      <Text>
-        Welcome to Property Analyser your one stop shop for property Data.
-      </Text>
-      <TouchableOpacity onPress={fetchPriceSearch} style={styles.button}>
-        <Text style={styles.buttonText}>search</Text>
-      </TouchableOpacity>
-      <VictoryChart
-      // width={moderateScale(320)}
-      // height={moderateScaleVertical(220)}
-      >
-        <VictoryGroup offset={15} colorScale={"qualitative"}>
-          <VictoryBar
-            data={[
-              { x: 1, y: 1 },
-              { x: 2, y: 3 },
-              { x: 3, y: 3 },
-            ]}
-            animate={{
-              duration: 1000,
-              onLoad: { duration: 1000 },
-            }}
-            cornerRadius={6}
-            style={{
-              data: {
-                // fill: colors.skyBle,
-              },
-            }}
-            barWidth={10}
-          />
+  if (isLoaded) {
+    return (
+      <View style={styles.mainContainer}>
+        <PropertyLogo />
+        <Text>
+          Welcome to Property Analyser your one stop shop for property Data.
+        </Text>
+        <TouchableOpacity onPress={fetchPriceSearch} style={styles.button}>
+          <Text style={styles.buttonText}>search</Text>
+        </TouchableOpacity>
 
-          <VictoryBar
-            data={[
-              { x: 1, y: 1 },
-              { x: 2, y: 2 },
-              { x: 3, y: 3 },
-            ]}
-            animate={{
-              duration: 2000,
-              onLoad: { duration: 1000 },
-            }}
-            cornerRadius={6}
-            style={{
-              data: {
-                // fill: colors.primary,
-              },
-            }}
-            barWidth={10}
-            labelComponent={CenteredLabel}
-          />
-          <VictoryAxis
-            tickValues={["70% Range", "Aug", "Sep"]}
-            style={{
-              tickLabels: {
-                fontSize: 5,
-                padding: 1,
-                angle: 10,
-                verticalAnchor: "middle",
-                textAnchor: "start",
-              },
-              //   tickLabels: { color: colors.blackL },
-              //   ticks: { color: colors.blackL },
-            }}
-          />
-          <VictoryAxis
-            dependentAxis={true}
-            tickValues={["₹0K", "₹10K", "₹20K", "₹30K"]}
-            style={
-              {
-                //   tickLabels: { fill: colors.blackL },
-                //   ticks: { stroke: colors.blackL },
-              }
-            }
-          />
-        </VictoryGroup>
-      </VictoryChart>
-    </View>
-  );
+        <View style={styles.graphContainer}>
+          <VictoryChart
+            height={chartHeight}
+            width={chartWidth}
+            padding={{ left: 60, bottom: 30, right: 30 }}
+          >
+            <VictoryGroup offset={15} colorScale={"qualitative"}>
+              <VictoryBar
+                data={[
+                  { x: 1, y: data.data["70pc_range"][0] },
+                  { x: 2, y: data.data["80pc_range"][0] },
+                  { x: 3, y: data.data["90pc_range"][0] },
+                ]}
+                animate={{
+                  duration: 1000,
+                  onLoad: { duration: 1000 },
+                }}
+                cornerRadius={6}
+                style={{
+                  data: {
+                    // fill: colors.skyBle,
+                  },
+                }}
+                barWidth={10}
+              />
+
+              <VictoryBar
+                data={[
+                  { x: 1, y: data.data["70pc_range"][1] },
+                  { x: 2, y: data.data["80pc_range"][1] },
+                  { x: 3, y: data.data["90pc_range"][1] },
+                ]}
+                animate={{
+                  duration: 2000,
+                  onLoad: { duration: 1000 },
+                }}
+                cornerRadius={6}
+                style={{
+                  data: {
+                    // fill: colors.primary,
+                  },
+                }}
+                barWidth={10}
+                labelComponent={CenteredLabel}
+              />
+              <VictoryAxis
+                tickValues={["70% Range", "80% Range", "90% Range"]}
+                style={{
+                  tickLabels: {
+                    fontSize: 7,
+                  },
+                  //   tickLabels: { color: colors.blackL },
+                  //   ticks: { color: colors.blackL },
+                }}
+              />
+              <VictoryAxis
+                dependentAxis
+                orientation="left"
+                style={{ tickLabels: { fontSize: 5 } }}
+                fixLabelOverlap={true}
+              />
+            </VictoryGroup>
+          </VictoryChart>
+        </View>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.mainContainer}>
+        <PropertyLogo />
+        <Text>
+          Welcome to Property Analyser your one stop shop for property Data.
+        </Text>
+        <TouchableOpacity onPress={fetchPriceSearch} style={styles.button}>
+          <Text style={styles.buttonText}>search</Text>
+        </TouchableOpacity>
+        {isLoaded.length ? (
+          <View style={styles.graphContainer}>
+            <Text>{data.data["70pc_range"][1]}</Text>
+            <VictoryChart height={chartHeight} width={chartWidth}>
+              <VictoryGroup offset={15} colorScale={"qualitative"}>
+                <VictoryBar
+                  data={[
+                    { x: 1, y: data.data["70pc_range"][0] },
+                    { x: 2, y: data.data["80pc_range"][0] },
+                    { x: 3, y: data.data["90pc_range"][0] },
+                  ]}
+                  animate={{
+                    duration: 1000,
+                    onLoad: { duration: 1000 },
+                  }}
+                  cornerRadius={6}
+                  style={{
+                    data: {
+                      // fill: colors.skyBle,
+                    },
+                  }}
+                  barWidth={10}
+                />
+
+                <VictoryBar
+                  data={[
+                    { x: 1, y: data.data["70pc_range"][1] },
+                    { x: 2, y: data.data["80pc_range"][1] },
+                    { x: 3, y: data.data["90pc_range"][1] },
+                  ]}
+                  animate={{
+                    duration: 2000,
+                    onLoad: { duration: 1000 },
+                  }}
+                  cornerRadius={6}
+                  style={{
+                    data: {
+                      // fill: colors.primary,
+                    },
+                  }}
+                  barWidth={10}
+                  labelComponent={CenteredLabel}
+                />
+                <VictoryAxis
+                  tickValues={["70% Range", "80% Range", "90% Range"]}
+                  style={{
+                    tickLabels: {
+                      fontSize: 2,
+                      padding: 1,
+                      angle: 10,
+                      verticalAnchor: "middle",
+                      textAnchor: "start",
+                    },
+                    axisLabel: {
+                      fontSize: 100,
+                    },
+                    //   tickLabels: { color: colors.blackL },
+                    //   ticks: { color: colors.blackL },
+                  }}
+                />
+                <VictoryAxis
+                  dependentAxis={true}
+                  style={
+                    {
+                      //   tickLabels: { fill: colors.blackL },
+                      //   ticks: { stroke: colors.blackL },
+                    }
+                  }
+                />
+              </VictoryGroup>
+            </VictoryChart>
+          </View>
+        ) : (
+          <Text>Is loading</Text>
+        )}
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
-  SVmainContainer: {
-    flex: 1,
-    flexDirection: "column",
-    marginTop: 50,
-  },
   mainContainer: {
     flex: 1,
     flexDirection: "column",
     marginTop: 50,
     alignItems: "center",
+    marginLeft: 0,
+  },
+  graphContainer: {
+    width: "100%",
+    paddingLeft: 5,
   },
 });
 
